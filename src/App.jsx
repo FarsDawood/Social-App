c38009ec10ea3684375e0916b4602622be92dd51
Fars Mostafa <fares.moostafa@gmail.com>
2026-02-08 05:09:39 +0200

    refactor: rename Resister folder to Register

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
-import Register from "./components/Resister/Register";
+import Register from "./components/Register/Register";
 import NotFound from "./components/NotFound/NotFound";
 
 export default function App() {
